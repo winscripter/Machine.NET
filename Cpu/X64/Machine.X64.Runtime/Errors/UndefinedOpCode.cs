d24ec1790a0e25b3c163d124c4a5b81f52c0ec81
VisualStudio2022User <75rxthebestguy@gmail.com>
2024-12-20 22:42:59 +0400

    Include UndefinedOpCode.cs

---
+++ Cpu/X64/Machine.X64.Runtime/Errors/UndefinedOpCode.cs
@@ -0,0 +1,10 @@
+﻿namespace Machine.X64.Runtime.Errors;
+
+internal sealed class UndefinedOpCode : IErrorRecord
+{
+    public string? DisplayName => "The operation code is unknown";
+
+    public ulong ErrorCode => 0;
+
+    public byte Interrupt => 6;
+}
