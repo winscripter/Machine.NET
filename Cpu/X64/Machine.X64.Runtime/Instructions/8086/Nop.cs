43e74923e7c62a11aeae990f2c2c395a65d1bbb4
VisualStudio2022User <75rxthebestguy@gmail.com>
2024-12-20 23:28:21 +0400

    Include Nop.cs

---
+++ Cpu/X64/Machine.X64.Runtime/Instructions/8086/Nop.cs
@@ -0,0 +1,24 @@
+﻿using Iced.Intel;
+
+namespace Machine.X64.Runtime;
+
+public partial class CpuRuntime
+{
+    private void nop(in Instruction instruction)
+    {
+        switch (instruction.Code)
+        {
+            case Code.Nopd:
+            case Code.Nopw:
+            case Code.Nopq:
+            case Code.Nop_rm16:
+            case Code.Nop_rm32:
+            case Code.Nop_rm64:
+                break;
+
+            default:
+                ReportInvalidCodeUnderMnemonic(instruction.Code, instruction.Mnemonic);
+                break;
+        }
+    }
+}
