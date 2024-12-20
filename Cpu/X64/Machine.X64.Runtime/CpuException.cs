f5fbe54bbf0c1a67648c7efb7ff0fd0759b6e8c2
VisualStudio2022User <75rxthebestguy@gmail.com>
2024-12-20 22:14:28 +0400

    Document CpuException

--- Cpu/X64/Machine.X64.Runtime/CpuException.cs
+++ Cpu/X64/Machine.X64.Runtime/CpuException.cs
@@ -5,14 +5,26 @@
 /// </summary>
 public sealed class CpuException : Exception
 {
+    /// <summary>
+    /// Initializes a new instance of the <see cref="CpuException"/> class.
+    /// </summary>
     public CpuException()
     {
     }
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="CpuException"/> class.
+    /// </summary>
+    /// <param name="message">Exception message (if any)</param>
     public CpuException(string? message) : base(message)
     {
     }
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="CpuException"/> class.
+    /// </summary>
+    /// <param name="message">Exception message (if any)</param>
+    /// <param name="innerException">Inner exception (if any)</param>
     public CpuException(string? message, Exception? innerException) : base(message, innerException)
     {
     }
