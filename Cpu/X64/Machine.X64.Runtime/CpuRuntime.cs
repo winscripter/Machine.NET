477ec1745ed2d2b52016dfc5eb7ed6189e106008
VisualStudio2022User <75rxthebestguy@gmail.com>
2024-12-21 13:06:13 +0400

    Fix GetControlMaskMData4 returning 4 bits instead of 64

--- Cpu/X64/Machine.X64.Runtime/CpuRuntime.cs
+++ Cpu/X64/Machine.X64.Runtime/CpuRuntime.cs
@@ -95,15 +95,10 @@ public sealed partial class CpuRuntime(int memorySize = 65536, int ioPortCount =
         set => ProcessorRegisters.CR0PE = value;
     }
 
-    //private ReadOnlySpan<bool> GetControlMaskMData4()
-    //{
-    //    return new ReadOnlySpan<bool>([
-    //        BitUtilities.IsBitSet(ControlMaskM, 0),
-    //        BitUtilities.IsBitSet(ControlMaskM, 1),
-    //        BitUtilities.IsBitSet(ControlMaskM, 2),
-    //        BitUtilities.IsBitSet(ControlMaskM, 3),
-    //    ]);
-    //}
+    private ReadOnlySpan<bool> GetControlMaskMData4()
+    {
+        return BitManager.GetBitsBigEndian(ControlMaskM);
+    }
 
     private void ProcessFlags(Int128 result)
     {
