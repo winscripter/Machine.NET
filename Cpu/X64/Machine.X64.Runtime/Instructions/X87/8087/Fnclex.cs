0d1102716a244e0e76c75ca5b55816d63eb7e0ae
VisualStudio2022User <75rxthebestguy@gmail.com>
2024-12-21 14:08:44 +0400

    Implement X87: FNCLEX (see #1)

---
+++ Cpu/X64/Machine.X64.Runtime/Instructions/X87/8087/Fnclex.cs
@@ -0,0 +1,23 @@
+﻿using Iced.Intel;
+using Machine.X64.Component;
+
+namespace Machine.X64.Runtime;
+
+public partial class CpuRuntime
+{
+    private void fnclex(in Instruction instruction)
+    {
+        switch (instruction.Code)
+        {
+            case Code.Fnclex:
+                {
+                    this.Fpu.MaskedExceptions = ExceptionMask.Clear;
+                    break;
+                }
+
+            default:
+                ReportInvalidCodeUnderMnemonic(instruction.Code, instruction.Mnemonic);
+                break;
+        }
+    }
+}
