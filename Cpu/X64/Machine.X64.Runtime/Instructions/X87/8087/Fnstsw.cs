705dcc83a3f43b6a5e099a0c22164514b6b8b5ba
VisualStudio2022User <75rxthebestguy@gmail.com>
2024-12-21 14:02:56 +0400

    Implement x87: FNSTSW (see #1)

---
+++ Cpu/X64/Machine.X64.Runtime/Instructions/X87/8087/Fnstsw.cs
@@ -0,0 +1,28 @@
+﻿using Iced.Intel;
+
+namespace Machine.X64.Runtime;
+
+public partial class CpuRuntime
+{
+    private void fnstsw(in Instruction instruction)
+    {
+        switch (instruction.Code)
+        {
+            case Code.Fnstsw_AX:
+                {
+                    this.ProcessorRegisters.Ax = this.Fpu.StatusWord;
+                    break;
+                }
+
+            case Code.Fnstsw_m2byte:
+                {
+                    this.Memory.WriteUInt16(GetMemOperand16(instruction), this.Fpu.StatusWord);
+                    break;
+                }
+
+            default:
+                ReportInvalidCodeUnderMnemonic(instruction.Code, instruction.Mnemonic);
+                break;
+        }
+    }
+}
