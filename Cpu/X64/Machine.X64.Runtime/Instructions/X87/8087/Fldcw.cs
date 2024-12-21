4ff169b078ac811b9b1f182e250302e7448a5d87
VisualStudio2022User <75rxthebestguy@gmail.com>
2024-12-21 13:34:56 +0400

    Implement x87: FLDCW

---
+++ Cpu/X64/Machine.X64.Runtime/Instructions/X87/8087/Fldcw.cs
@@ -0,0 +1,23 @@
+﻿using Iced.Intel;
+
+namespace Machine.X64.Runtime;
+
+public partial class CpuRuntime
+{
+    private void fldcw(in Instruction instruction)
+    {
+        switch (instruction.Code)
+        {
+            case Code.Fldcw_m2byte:
+                {
+                    ushort controlWord = this.Memory.ReadUInt16(GetMemOperand64(instruction));
+                    this.Fpu.ControlWord = controlWord;
+                    break;
+                }
+                
+            default:
+                ReportInvalidCodeUnderMnemonic(instruction.Code, instruction.Mnemonic);
+                break;
+        }
+    }
+}
