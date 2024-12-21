141217df2c9a9952d97a0099fc111159ad4baca0
VisualStudio2022User <75rxthebestguy@gmail.com>
2024-12-21 13:31:45 +0400

    Implement x87: FNINIT

---
+++ Cpu/X64/Machine.X64.Runtime/Instructions/X87/8087/Fninit.cs
@@ -0,0 +1,20 @@
+﻿using Iced.Intel;
+
+namespace Machine.X64.Runtime;
+
+public partial class CpuRuntime
+{
+    private void fninit(in Instruction instruction)
+    {
+        switch (instruction.Code)
+        {
+            case Code.Fninit:
+                this.Fpu.Reset();
+                break;
+
+            default:
+                ReportInvalidCodeUnderMnemonic(instruction.Code, instruction.Mnemonic);
+                break;
+        }
+    }
+}
