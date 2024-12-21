3b5b323679583a1b6dd3eff52f27b2c60ce2d0e2
VisualStudio2022User <75rxthebestguy@gmail.com>
2024-12-21 14:47:25 +0400

    Unify GetMemOperand[8/16/32/64] into one method & introduce the STR i286 instruction

---
+++ Cpu/X64/Machine.X64.Runtime/Instructions/286/Str.cs
@@ -0,0 +1,16 @@
+﻿using Iced.Intel;
+
+namespace Machine.X64.Runtime;
+
+public partial class CpuRuntime
+{
+    private void str(in Instruction instruction)
+    {
+        switch (instruction.Code)
+        {
+            default:
+                ReportInvalidCodeUnderMnemonic(instruction.Code, instruction.Mnemonic);
+                break;
+        }
+    }
+}
